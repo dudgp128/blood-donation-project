7c1ed708384037a0346d97fbe16090c538f74849
dudgp128 <20181048@sungshin.ac.kr>
2023-09-26 21:24:52 +0900

    [FEAT] CSV 형태에 맞게 데이터를 배열로 분할하기

--- src/chart/base.js
+++ src/chart/base.js
@@ -1,7 +1,33 @@
 import Papa from "papaparse";
 
+const isRowCSV = (valueArray) => {
+  return valueArray[0] === valueArray[1];
+};
+
+const rowCSV = (csv) => {
+  const column = Object.values(csv[0])[0];
+  let values = Object.values(csv[1]);
+  return { column, values };
+};
+
+const columnCSV = (csv) => {
+  const year = [];
+  const people = [];
+  const time = [];
+
+  for (let i of csv) {
+    let [year_data, people_data, time_data] = Array.from(Object.values(i));
+    year.push(year_data);
+    people.push(people_data);
+    time.push(time_data);
+  }
+
+  return { year, people, time };
+};
+
 const fetchData = async (filename) => {
   let csvData = [];
+  let data = [];
   try {
     const response = await fetch(process.env.PUBLIC_URL + "/data/" + filename);
 
@@ -12,13 +38,16 @@ const fetchData = async (filename) => {
       dynamicTyping: true,
       complete: (result) => {
         csvData = result.data;
+        data = isRowCSV(Object.values(csvData[0]))
+          ? rowCSV(csvData)
+          : columnCSV(csvData);
       },
     });
   } catch (error) {
     console.error("데이터 가져오기 오류:", error);
   }
 
-  return csvData;
+  return data;
 };
 
 export default fetchData;
