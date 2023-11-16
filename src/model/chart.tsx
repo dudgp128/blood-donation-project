ec58cbf54862bed4b81b65b60e34eecddc4edf4f
Yeonghye <yhjo@kaltour.com>
2023-11-16 17:48:47 +0900

    [TYPE] 타입 선언하기 +mapKorea, Region_Data

--- src/model/chart.tsx
+++ src/model/chart.tsx
@@ -20,3 +20,27 @@ export type Titles={
     titleContent:string;
     titleExplain:string;
 }
+
+export interface mapKorea{
+    'kr-so':string,
+    'kr-kg': string,
+    'kr-gb' :string,
+    'kr-gn' : string,
+    'kr-cb' : string,
+    'kr-kn' : string,
+    'kr-2685':string,
+    'kr-pu':string,
+    'kr-tg':string,
+    'kr-2688':string,
+    'kr-sj':string,
+    'kr-tj':string,
+    'kr-ul':string,
+    'kr-in':string,
+    'kr-kw':string,
+    'kr-cj':string,
+    'kr-kj':string
+}
+
+export interface Region_Data{
+    data:{ year:number[], region:string[], count:number[], percent:number[]};
+}
