e52ddf4feee0299b2029ed2ac36e21194dfbb69c
Yeonghye <yhjo@kaltour.com>
2023-11-17 14:18:15 +0900

    [FEAT] Region 데이터, 차트 생성하기

--- src/chart/REGION_BLOOD/Chart.tsx
+++ src/chart/REGION_BLOOD/Chart.tsx
@@ -1,8 +1,8 @@
 import Highcharts, { SeriesOptionsType } from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import mapDataAsia from "./mapSouthKorea";
-import { Region_Data } from "../../model/chart";
-import { useState } from "react";
+import { Region_Data, MapKorea } from "../../model/chart";
+import { useState, useEffect } from "react";
 import fetchData from "../base";
 
 
@@ -10,101 +10,110 @@ import fetchData from "../base";
 require("highcharts/modules/map")(Highcharts);
 
 // Render app with demo chart
-const MyMapComponent:React.FC=()=> {
+const MyMapComponent: React.FC = () => {
 
-    const [dataSet, setDataSet] = useState<Region_Data>({
-      data:{ year:[], region:[], count:[], percent:[]},
-    });
+  const [dataSet, setDataSet] = useState<Region_Data>({
+    data: { year: [], region: [], count: [], percent: [] },
+  });
 
-    const fetchDataAndUpdate = async (filename: string, key: keyof Region_Data): Promise<void> => {
-      try {
-        let result: any = await fetchData(filename, Object.keys(dataSet[key]));
-        setDataSet((prevData) => ({
-          ...prevData,
-          [key]: result,
-        }));
-        console.log(dataSet);
-      } catch (error) {
-        console.error(`데이터 가져오기 오류 (${key}):`, error);
-      }
-    };
-  
-    const fetchDataAsync = async ():Promise<void> => {
-      await fetchDataAndUpdate("시·도별_인구대비_헌혈실적_20231116141055.csv", "data");
-    };
+  const fetchDataAndUpdate = async (filename: string, key: keyof Region_Data): Promise<void> => {
+    try {
+      let result: any = await fetchData(filename, Object.keys(dataSet[key]));
+      setDataSet((prevData) => ({
+        ...prevData,
+        [key]: result,
+      }));
+    } catch (error) {
+      console.error(`데이터 가져오기 오류 (${key}):`, error);
+    }
+  };
 
-    // var data = [
-    //     ['kr-4194', 10], ['kr-kg', 11], ['kr-cb', 12], ['kr-kn', 13],
-    //     ['kr-2685', 14], ['kr-pu', 15], ['kr-2688', 16], ['kr-sj', 17],
-    //     ['kr-tj', 18], ['kr-ul', 19], ['kr-in', 20], ['kr-kw', 21],
-    //     ['kr-gn', 22], ['kr-cj', 23], ['kr-gb', 24], ['kr-so', 25],
-    //     ['kr-tg', 26], ['kr-kj', 27]
-    // ];
+  const fetchDataAsync = async (): Promise<void> => {
+    await fetchDataAndUpdate("시·도별_인구대비_헌혈실적_20231116141055.csv", "data");
+  };
 
-    // const cityName:mapKorea={
-    //     'kr-so':'서울',
-    //     'kr-kg': '경기',
-    //     'kr-gb' :'충북',
-    //     'kr-gn' : '충남',
-    //     'kr-cb' : '전북',
-    //     'kr-kn' : '경남',
-    //     'kr-2685':'전남',
-    //     'kr-pu':'부산',
-    //     'kr-tg':'대구',
-    //     'kr-2688':'경북',
-    //     'kr-sj':'세종',
-    //     'kr-tj':'대전',
-    //     'kr-ul':'울산',
-    //     'kr-in':'인천',
-    //     'kr-kw':'강원',
-    //     'kr-cj':'제주',
-    //     'kr-kj':'광주',
-    //   }
+  useEffect(() => {
+    fetchDataAsync();
+    finaldData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-      const series:SeriesOptionsType[]= [
-        {
-             type:'map', // 필수값?
-             mapData: mapDataAsia,
-             data: dataSet.data,
-             dataLabels: {
-               enabled: true,
-               format:{this.point.name} // this.properties.hc-key로 수정
-               }
-           }
-      ]
-    
-    const options:Highcharts.Options = {
-      title: {
-        text: ""
-      },
-      credits: {
-        href: "https://www.bloodinfo.net/main.do",
-        text: " *데이터 출처: 대한적십자사 「혈액정보통계」 ",
-        style: {
-          color: "#707070",
-        },
-      },
-      colorAxis: {
-        min: 0,
-      },
-    
-      mapNavigation: {
+
+  const citycode: MapKorea = {
+    합계: 'kr-4194',
+    서울: 'kr-so',
+    경기: 'kr-kg',
+    충북: 'kr-gb',
+    대전·세종·충남: 'kr-gn',
+    전북: 'kr-cb',
+    경남: 'kr-kn',
+    광주·전남: 'kr-2685',
+    부산: 'kr-pu',
+    대구·경북: 'kr-2688',
+    울산: 'kr-ul',
+    인천: 'kr-in',
+    강원: 'kr-kw',
+    제주: 'kr-cj',
+  }
+
+  const finaldData = (): [string, number][] => {
+    const { region, percent } = dataSet.data;
+
+    const code_region = region.map((e) => citycode[e]);
+
+    if (code_region.length !== percent.length) {
+      throw new Error('Arrays must have the same length');
+    }
+
+    return code_region.map((value, index) => [value, percent[index]]);
+  }
+
+  const series: SeriesOptionsType[] = [
+    {
+      type: 'map', // 필수값?
+      mapData: mapDataAsia,
+      data: finaldData(),
+      dataLabels: {
         enabled: true,
-        buttonOptions: {
-            verticalAlign: 'bottom'
-        }
+        format: '{point.hc-key}' // this.properties.hc-key로 수정
+      }
+    }
+  ]
+
+  const options: Highcharts.Options = {
+    title: {
+      text: ""
+    },
+    credits: {
+      href: "https://www.bloodinfo.net/main.do",
+      text: " *데이터 출처: 대한적십자사 「혈액정보통계」 ",
+      style: {
+        color: "#707070",
+      },
+    },
+    colorAxis: {
+      min: 0,
+      max: 9
     },
 
-    };
-    return (
-      <div>
-        <HighchartsReact
-          options={options}
-          constructorType={"mapChart"}
-          highcharts={Highcharts}
-        />
-      </div>
-    );
-  
+    mapNavigation: {
+      enabled: true,
+      buttonOptions: {
+        verticalAlign: 'bottom'
+      }
+    },
+    series: series
+  };
+
+  return (
+    <div>
+      <HighchartsReact
+        options={options}
+        constructorType={"mapChart"}
+        highcharts={Highcharts}
+      />
+    </div>
+  );
+
 }
 export default MyMapComponent;
--- src/model/chart.tsx
+++ src/model/chart.tsx
@@ -3,44 +3,44 @@ export interface Data {
     donation_people: { year: number[], people: number[], time: number[] },
 }
 
-export type Result={
-    value:number,
-    color:string
+export type Result = {
+    value: number,
+    color: string
 }[]
 
 
-export type Tooltip={
-    year:number,
-    circle_color:string,
-    column:string,
-    value:string | object
+export type Tooltip = {
+    year: number,
+    circle_color: string,
+    column: string,
+    value: string | object
 }
 
-export type Titles={
-    titleContent:string;
-    titleExplain:string;
+export type Titles = {
+    titleContent: string;
+    titleExplain: string;
 }
 
-export interface mapKorea{
-    'kr-so':string,
-    'kr-kg': string,
-    'kr-gb' :string,
-    'kr-gn' : string,
-    'kr-cb' : string,
-    'kr-kn' : string,
-    'kr-2685':string,
-    'kr-pu':string,
-    'kr-tg':string,
-    'kr-2688':string,
-    'kr-sj':string,
-    'kr-tj':string,
-    'kr-ul':string,
-    'kr-in':string,
-    'kr-kw':string,
-    'kr-cj':string,
-    'kr-kj':string
+export interface MapKorea {
+    합계: string;
+    서울: string;
+    경기: string;
+    충북: string;
+    대전·세종·충남: string;
+    전북: string;
+    경남: string;
+    광주·전남: string;
+    부산: string;
+    대구·경북: string;
+    울산: string;
+    인천: string;
+    강원: string;
+    제주: string;
+
+    [key: string]: string; // Index signature
 }
 
-export interface Region_Data{
-    data:{ year:number[], region:string[], count:number[], percent:number[]};
+
+export interface Region_Data {
+    data: { year: number[], region: string[], count: number[], percent: number[] };
 }
