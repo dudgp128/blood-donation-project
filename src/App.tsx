2f8164ee596474f06f788085c4839ef6221a9dff
dudgp128 <20181048@sungshin.ac.kr>
2023-11-30 00:35:26 +0900

    npm install @type/react-virtualized

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,34 @@ import styled from "styled-components";
 import Header from "./components/Header";
 import Cover from "./containers/Cover";
 import ChartContainer from "./containers/ChartContainer";
-
+import {
+  WindowScroller,
+  CellMeasurer,
+  CellMeasurerCache,
+  AutoSizer,
+  List,
+  ListRowProps,
+} from "react-virtualized";
 
 const Contents = styled.div`
   display: flex;
   flex-direction: column;
   position: relative;
   top: 100px;
-  gap:100px;
+  gap: 100px;
 `;
 
-const App:React.FC=()=> {
+const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
       <Contents>
         <Cover />
-        <ChartContainer category={'BLOOD_SUPPLIED_NUMBER_DONORS'}/>
-        <ChartContainer category={'REGION_BLOOD'}/>
+        <ChartContainer category={"BLOOD_SUPPLIED_NUMBER_DONORS"} />
+        <ChartContainer category={"REGION_BLOOD"} />
       </Contents>
     </div>
   );
-}
+};
 
 export default App;
